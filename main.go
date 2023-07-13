64628f9f912ff9f38e235a8974c331af5ef49066
Andrew Snodgrass <git@pointlander.info>
2023-07-12 22:45:00 -0500

    WIP numbers

--- main.go
+++ main.go
@@ -5,13 +5,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
 	"math/rand"
 	"sort"
 )
 
+var (
+	// FlagBits bits mode
+	FlagBits = flag.Bool("bits", false, "bits mode")
+	// FlagNumbers numbers mode
+	FlagNumbers = flag.Bool("numbers", false, "numbers mode")
+)
+
 func main() {
+	flag.Parse()
+
+	if *FlagBits {
+		Bits()
+		return
+	}
+
+	if *FlagNumbers {
+		Numbers()
+		return
+	}
+}
+
+func Bits() {
 	rnd := rand.New(rand.NewSource(1))
 	type Distribution struct {
 		Mean   float64
@@ -132,6 +154,90 @@ func main() {
 		sd = samples[1].Sd
 		a = samples[1].DeltasA
 		b = samples[1].DeltasB
+		fmt.Println(j, avg, sd)
+		j++
+	}
+	fmt.Println(a)
+	fmt.Println(b)
+	shownum(a)
+	shownum(b)
+}
+
+func Numbers() {
+	rnd := rand.New(rand.NewSource(1))
+	type Distribution struct {
+		Mean   float64
+		StdDev float64
+	}
+	target := 77
+	n := int(math.Ceil(math.Log2(float64(target))))
+	a := Distribution{Mean: 0, StdDev: math.Ceil(math.Sqrt(float64(target)))}
+	b := Distribution{Mean: 0, StdDev: math.Ceil(math.Sqrt(float64(target)))}
+	fmt.Println(n)
+	fmt.Println(a)
+	fmt.Println(b)
+	shownum := func(a Distribution) {
+		fmt.Println(a.Mean)
+	}
+	samples := 8 * 1024
+	sample := func(a, b Distribution, ia, ib int, d float64) (avg, sd float64) {
+		i := 0
+		for i < samples {
+			x := int((rnd.NormFloat64() + a.Mean) * a.StdDev)
+			y := int((rnd.NormFloat64() + b.Mean) * b.StdDev)
+			xx := 0
+			if x > 0 {
+				xx = target % x
+			}
+			yy := 0
+			if y > 0 {
+				yy = target % y
+			}
+			cost := target - x*y
+			if cost < 0 {
+				cost = -cost
+			}
+			cost += yy + xx
+			avg += float64(cost)
+			sd += float64(cost) * float64(cost)
+			i += 1
+		}
+		avg /= float64(samples)
+		sd = math.Sqrt(sd/float64(samples) - avg*avg)
+		return avg, sd
+	}
+
+	avg, sd := sample(a, b, -1, -1, 0)
+	j := 0
+	for j < 1000 {
+		if sd == 0 {
+			break
+		}
+		type Sample struct {
+			DeltasA Distribution
+			DeltasB Distribution
+			Avg     float64
+			Sd      float64
+		}
+		samples := make([]Sample, 16)
+		for i := range samples {
+			samples[i].DeltasA = Distribution{Mean: a.Mean + rnd.NormFloat64(), StdDev: a.StdDev + rnd.NormFloat64()}
+			if samples[i].DeltasA.StdDev < 0 {
+				samples[i].DeltasA.StdDev = -samples[i].DeltasA.StdDev
+			}
+			samples[i].DeltasB = Distribution{Mean: b.Mean + rnd.NormFloat64(), StdDev: b.StdDev + rnd.NormFloat64()}
+			if samples[i].DeltasB.StdDev < 0 {
+				samples[i].DeltasB.StdDev = -samples[i].DeltasB.StdDev
+			}
+			samples[i].Avg, samples[i].Sd = sample(samples[i].DeltasA, samples[i].DeltasB, -1, -1, 0)
+		}
+		sort.Slice(samples, func(i, j int) bool {
+			return samples[i].Avg < samples[j].Avg
+		})
+		avg = samples[0].Avg
+		sd = samples[0].Sd
+		a = samples[0].DeltasA
+		b = samples[0].DeltasB
 		fmt.Println(j, avg, sd)
 		j++
 	}
