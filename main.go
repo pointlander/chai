f90988f9fe63b7a04d91b44dd9c61f349691708a
Andrew Snodgrass <git@pointlander.info>
2023-08-02 22:50:22 -0500

    Added GA for rnn model

--- main.go
+++ main.go
@@ -134,12 +134,14 @@ func main() {
 	}
 
 	{
+		cpus := runtime.NumCPU()
 		rng := rand.New(rand.NewSource(int64(1)))
 		type Distribution struct {
 			Mean   float64
 			StdDev float64
 		}
-		cols, rows := 32, 32
+		const pop = 128
+		const cols, rows = 32, 32
 
 		shownum := func(a []Distribution) int {
 			x := 0
@@ -152,7 +154,6 @@ func main() {
 			}
 			return x
 		}
-		_ = shownum
 
 		type Genome struct {
 			A       []Distribution
@@ -162,49 +163,70 @@ func main() {
 			Fitness float64
 			Cached  bool
 		}
+		pool := make([]Genome, 0, pop)
 		target := *FlagTarget
 		size := int(math.Ceil(math.Log2(float64(target))))
 		n := int(math.Ceil(math.Log2(math.Sqrt(float64(target)))))
 
 		factor := math.Sqrt(2.0 / float64(cols))
-		weights := make([]Distribution, 0, cols*rows)
-		for i := 0; i < cols*rows; i++ {
-			weights = append(weights, Distribution{Mean: factor * rnd.NormFloat64(), StdDev: factor * rnd.NormFloat64()})
-		}
-		bias := make([]Distribution, 0, rows)
-		for i := 0; i < rows; i++ {
-			bias = append(bias, Distribution{Mean: factor * rnd.NormFloat64(), StdDev: factor * rnd.NormFloat64()})
-		}
-		a := make([]Distribution, 0, n)
-		for i := 0; i < n; i++ {
-			a = append(a, Distribution{Mean: rnd.NormFloat64(), StdDev: rnd.NormFloat64()})
-		}
-		t := make([]Distribution, 0, size)
-		for i := 0; i < size; i++ {
-			t = append(t, Distribution{Mean: rnd.NormFloat64(), StdDev: rnd.NormFloat64()})
-		}
-		g := Genome{
-			A:       a,
-			T:       t,
-			Weights: weights,
-			Bias:    bias,
+		for i := 0; i < pop; i++ {
+			weights := make([]Distribution, 0, cols*rows)
+			for i := 0; i < cols*rows; i++ {
+				weights = append(weights, Distribution{Mean: factor * rnd.NormFloat64(), StdDev: factor * rnd.NormFloat64()})
+			}
+			bias := make([]Distribution, 0, rows)
+			for i := 0; i < rows; i++ {
+				bias = append(bias, Distribution{Mean: factor * rnd.NormFloat64(), StdDev: factor * rnd.NormFloat64()})
+			}
+			a := make([]Distribution, 0, n)
+			for i := 0; i < n; i++ {
+				a = append(a, Distribution{Mean: rnd.NormFloat64(), StdDev: rnd.NormFloat64()})
+			}
+			t := make([]Distribution, 0, size)
+			for i := 0; i < size; i++ {
+				t = append(t, Distribution{Mean: rnd.NormFloat64(), StdDev: rnd.NormFloat64()})
+			}
+			g := Genome{
+				A:       a,
+				T:       t,
+				Weights: weights,
+				Bias:    bias,
+			}
+			pool = append(pool, g)
 		}
 
-		sample := func(g *Genome) (samples plotter.Values, avg, stddev float64) {
+		copy := func(g *Genome) Genome {
+			a := make([]Distribution, len(g.A))
+			copy(a, g.A)
+			t := make([]Distribution, len(g.T))
+			copy(t, g.T)
+			weights := make([]Distribution, len(g.Weights))
+			copy(weights, g.Weights)
+			bias := make([]Distribution, len(g.Bias))
+			copy(bias, g.Bias)
+			return Genome{
+				A:       a,
+				T:       t,
+				Weights: weights,
+				Bias:    bias,
+			}
+		}
+
+		sample := func(rng *rand.Rand, g *Genome) (samples plotter.Values, avg, stddev float64) {
 			in := make([]Distribution, 0, 8)
 			in = append(in, g.A...)
 			in = append(in, g.T...)
 			layer := NewMatrix(0, cols, rows)
-			for _, w := range weights {
+			for _, w := range g.Weights {
 				layer.Data = append(layer.Data, (rng.NormFloat64()+w.Mean)*w.StdDev)
 			}
 			b := NewMatrix(0, 1, rows)
-			for _, w := range bias {
+			for _, w := range g.Bias {
 				b.Data = append(b.Data, (rng.NormFloat64()+w.Mean)*w.StdDev)
 			}
 			inputs := NewMatrix(0, cols, 1)
 			for i := 0; i < cols; i++ {
-				if rnd.Intn(2) == 0 {
+				if rng.Intn(2) == 0 {
 					inputs.Data = append(inputs.Data, 1)
 				} else {
 					inputs.Data = append(inputs.Data, -1)
@@ -212,10 +234,10 @@ func main() {
 			}
 			var state uint64
 			cost, total := 0.0, 0.0
-			for i := 0; i < 1024; i++ {
+			for i := 0; i < 16; i++ {
 				sampledA := int64(0)
 				e := int64(1)
-				for _, v := range a {
+				for _, v := range g.A {
 					if (rng.NormFloat64()+v.Mean)*v.StdDev > 0 {
 						sampledA += e
 					}
@@ -226,7 +248,7 @@ func main() {
 				}
 				sampledT := int64(0)
 				e = int64(1)
-				for _, v := range t {
+				for _, v := range g.T {
 					if (rng.NormFloat64()+v.Mean)*v.StdDev > 0 {
 						sampledT += e
 					}
@@ -272,13 +294,18 @@ func main() {
 			cost /= (total * float64(len(g.A)*len(g.T)))
 			return samples, cost, 0
 		}
-		samples, avg, stddev := sample(&g)
-		fmt.Println(avg, stddev)
+		d := make(plotter.Values, 0, 8)
+		for i := range pool {
+			dd, avg, _ := sample(rng, &pool[i])
+			pool[i].Fitness = avg
+			pool[i].Cached = true
+			d = append(d, dd...)
+		}
 
 		p := plot.New()
 		p.Title.Text = "rnn"
 
-		histogram, err := plotter.NewHist(samples, 10)
+		histogram, err := plotter.NewHist(d, 10)
 		if err != nil {
 			panic(err)
 		}
@@ -287,6 +314,103 @@ func main() {
 		err = p.Save(8*vg.Inch, 8*vg.Inch, "rnn.png")
 		if err != nil {
 			panic(err)
+		}
+
+	Search:
+		for {
+			sort.Slice(pool, func(i, j int) bool {
+				return pool[i].Fitness < pool[j].Fitness
+			})
+			pool = pool[:pop]
+			fmt.Println(pool[0].Fitness)
+			for i := range pool {
+				x := shownum(pool[i].A)
+				if x == 0 {
+					continue
+				}
+				if target%x == 0 {
+					if x == 1 || x == target {
+						continue
+					} else {
+						fmt.Println(x, target/x)
+						break Search
+					}
+				}
+			}
+			if pool[0].Fitness < 1e-16 {
+				break Search
+			}
+			for i := 0; i < pop/4; i++ {
+				for j := 0; j < pop/4; j++ {
+					if i == j {
+						continue
+					}
+					g := copy(&pool[i])
+					aa := pool[j].A
+					tt := pool[j].T
+					w := pool[j].Weights
+					b := pool[j].Bias
+					g.A[rnd.Intn(len(g.A))].Mean = aa[rnd.Intn(len(aa))].Mean
+					g.A[rnd.Intn(len(g.A))].StdDev = aa[rnd.Intn(len(aa))].StdDev
+					g.T[rnd.Intn(len(g.T))].Mean = tt[rnd.Intn(len(tt))].Mean
+					g.T[rnd.Intn(len(g.T))].StdDev = tt[rnd.Intn(len(tt))].StdDev
+					g.Weights[rnd.Intn(len(g.Weights))].Mean = w[rnd.Intn(len(w))].Mean
+					g.Weights[rnd.Intn(len(g.Weights))].StdDev = w[rnd.Intn(len(w))].StdDev
+					g.Bias[rnd.Intn(len(g.Bias))].Mean = b[rnd.Intn(len(b))].Mean
+					g.Bias[rnd.Intn(len(g.Bias))].StdDev = b[rnd.Intn(len(b))].StdDev
+					pool = append(pool, g)
+				}
+			}
+			for i := 0; i < pop; i++ {
+				g := copy(&pool[i])
+				g.A[rnd.Intn(len(g.A))].Mean += rnd.NormFloat64()
+				g.A[rnd.Intn(len(g.A))].StdDev += rnd.NormFloat64()
+				g.T[rnd.Intn(len(g.T))].Mean += rnd.NormFloat64()
+				g.T[rnd.Intn(len(g.T))].StdDev += rnd.NormFloat64()
+				g.Weights[rnd.Intn(len(g.Weights))].Mean += rnd.NormFloat64()
+				g.Weights[rnd.Intn(len(g.Weights))].StdDev += rnd.NormFloat64()
+				g.Bias[rnd.Intn(len(g.Bias))].Mean += rnd.NormFloat64()
+				g.Bias[rnd.Intn(len(g.Bias))].StdDev += rnd.NormFloat64()
+				pool = append(pool, g)
+			}
+			done := make(chan *rand.Rand, 8)
+			i, flight := 0, 0
+			rngs := make([]*rand.Rand, cpus)
+			for i := range rngs {
+				rngs[i] = rand.New(rand.NewSource(int64(i + 1)))
+			}
+			task := func(rng *rand.Rand, i int) {
+				_, avg, _ := sample(rng, &pool[i])
+				pool[i].Fitness = avg
+				pool[i].Cached = true
+				done <- rng
+			}
+			for i < len(pool) && flight < cpus {
+				if pool[i].Cached {
+					i++
+					continue
+				}
+				go task(rngs[flight], i)
+				i++
+				flight++
+			}
+			for i < len(pool) {
+				rng := <-done
+				flight--
+
+				if pool[i].Cached {
+					i++
+					continue
+				}
+
+				go task(rng, i)
+				i++
+				flight++
+			}
+			for flight > 0 {
+				<-done
+				flight--
+			}
 		}
 	}
 
